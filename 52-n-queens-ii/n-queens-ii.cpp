8ad5d1b58a3f95f45d2646c148a085669559b68c
Sk Saif Ali Haque <saifalii8771@gmail.com>
2026-09-05 00:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (68.44%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int count = 0;
+
+    void solve(int row, int n,
+               vector<bool>& col,
+               vector<bool>& diag1,
+               vector<bool>& diag2) {
+        
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+            // Check column and both diagonals
+            if (col[c] || diag1[row - c + n - 1] || diag2[row + c])
+                continue;
+
+            // Place queen
+            col[c] = true;
+            diag1[row - c + n - 1] = true;
+            diag2[row + c] = true;
+
+            solve(row + 1, n, col, diag1, diag2);
+
+            // Backtrack
+            col[c] = false;
+            diag1[row - c + n - 1] = false;
+            diag2[row + c] = false;
+        }
+    }
+    int totalNQueens(int n) {
+        vector<bool> col(n, false);
+        vector<bool> diag1(2 * n - 1, false);
+        vector<bool> diag2(2 * n - 1, false);
+
+        solve(0, n, col, diag1, diag2);
+
+        return count;
+    }
+};
